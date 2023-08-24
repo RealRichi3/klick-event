855892e7a39b1dd34cb8cf5b978ba7b6e0ca8d54
Joshua <cutejosh2@gmail.com>
2023-08-24 17:41:48 +0100

    deploy page

---
+++ src/App.jsx
@@ -0,0 +1,135 @@
+import logo from "./assets/logo.png"
+import seminars from "./assets/seminars.png"
+import workshop from "./assets/workshop.png"
+import kids from "./assets/kids.png"
+import image1 from './assets/image1.png'
+import image2 from './assets/image2.png'
+import image3 from './assets/image3.png'
+
+import greet from "./assets/greeting.png"
+import { Icons } from "./components/Icons"
+
+
+export default function App() {
+    const goToWhatsApp = () => {
+        window.location.href = 'https://wa.me/2347010876015';
+    }
+    const goToKlick = () => {
+        window.location.href = 'https://www.klick.africa/en/store/The-KLICK-Event-Store/1521205'
+    }
+
+    return (
+        <>
+            <main className="flex flex-col items-center justify-center">
+                <div className="relative mb-28 mt-10">
+                    {/* klick shop image */}
+                    <img src={logo} className="aspect-square" />
+                    <p className="italic text-center font-bold text-[#0186eb]">Making Parenting a Breeze...</p>
+                </div>
+
+                <div className="bg-[#0186eb] rounded-t-[50%] w-full">
+                    <div className="flex flex-col justify-center max-w-[1200px] space-y-8 pb-8 mx-auto -mt-20">
+                        {/* 3 images section */}
+                        <div className="flex items-center justify-center space-x-[50px] lg:space-x-[80px]">
+                            <img src={image1} className="rounded-t-[50%] rounded-b-[50%] border-[15px] border-white aspect-square h-[300px]" />
+
+                            <img src={image2} className="rounded-t-[50%] rounded-b-[50%] border-[15px] border-white aspect-square h-[400px]" />
+
+                            <img src={image3} className="rounded-t-[50%] rounded-b-[50%] border-[15px] border-white aspect-square h-[300px]" />
+                        </div>
+
+                        <h5 className="mt-40 md:mt-36 lg:mt-16 font-bold text-[20px] text-center">Come Shop Toys, Books, Baby Gear, Children fashion, <br />
+                            Women Fashion and Wellness.. and much more.</h5>
+
+
+                        <div className="flex items-center justify-center space-x-20 lg:space-x-32">
+                            <div className="flex flex-col items-center">
+                                <div className="rounded-t-[50%] rounded-b-[50%] border-[5px] border-yellow-400">
+                                    <img src={seminars} className="aspect-square h-[100px]" />
+                                </div>
+                                <p>Seminars</p>
+                            </div>
+                            <div className="flex flex-col items-center">
+                                <div className="rounded-t-[50%] rounded-b-[50%] border-[5px] border-yellow-400">
+                                    <img src={workshop} className="aspect-square h-[100px]" />
+                                </div>
+                                <p>Interactive workshop</p>
+                            </div>
+                            <div className="flex flex-col items-center">
+                                <div className="rounded-t-[50%] rounded-b-[50%] border-[5px] border-yellow-400">
+                                    <img src={kids} className="aspect-square h-[100px]" />
+                                </div>
+                                <p>Kids Play Area</p>
+                            </div>
+                        </div>
+
+                        <h5 className="mt-16 font-bold text-[20px] text-center">Saturday, October 28th | 10am - 6pm <br />
+                            The Condo - No. 1, Kofo Abayomi Street, VI, Lagos.</h5>
+
+                        <h4 className="text-yellow-400 text-3xl font-semibold text-center mt-16 mb-10">TABLE RESERVATION COSTING</h4>
+
+                        <div className="flex items-center justify-center space-x-5">
+                            {/* cards */}
+                            <div className="rounded-[50px] border flex flex-col justify-between space-y-10 mx-auto p-8 items-center text-center min-h-[500px] w-[388px]">
+                                <h1 className="border px-4 py-2 hover:cursor-pointer" onClick={goToKlick} >SILVER</h1>
+                                <div className="space-y-4">
+                                    <p>6 x 2 table</p>
+                                    <p>2 products images <br />
+                                        & logo featured on social media</p>
+                                </div>
+                                <h1 className="text-yellow-400 text-2xl font-semibold">N19,000</h1>
+                            </div>
+
+                            <div className="rounded-[50px] border flex flex-col justify-between space-y-10 mx-auto p-8 items-center text-center min-h-[500px] w-[388px]">
+                                <h1 className="border px-4 py-2 hover:cursor-pointer" onClick={goToKlick} >PLATINUM</h1>
+                                <div className="space-y-4">
+                                    <p>6 x 2 table</p>
+                                    <p>2 products images <br />
+                                        & logo featured on social media</p>
+                                    <p>Front row placement</p>
+                                    <p>30 minutes professional product photography</p>
+                                    <p>1 Instagram paid advert</p>
+                                </div>
+                                <h1 className="text-yellow-400 text-2xl font-semibold">N60,000</h1>
+                            </div>
+
+                            <div className="rounded-[50px] border flex flex-col justify-between space-y-10 mx-auto p-8 items-center text-center min-h-[500px] w-[388px]">
+                                <h1 className="border px-4 py-2 hover:cursor-pointer" onClick={goToKlick} >GOLD</h1>
+                                <div className="space-y-4">
+                                    <p>6 x 2 table</p>
+                                    <p>2 products images <br />
+                                        & logo featured on social media</p>
+                                    <p>Premium placement</p>
+                                    <p>15 minutes professional product photography</p>
+                                </div>
+                                <h1 className="text-yellow-400 text-2xl font-semibold">N39,000</h1>
+                            </div>
+                        </div>
+
+                        <h5 className="font-bold text-[20px] text-center">To be a seller, click <a href="" className="text-yellow-400">here</a> to purchase a table</h5>
+
+                        <h5 className="font-semibold text-[20px] text-center">For further information, contact details below</h5>
+                    </div>
+                </div>
+
+                <footer className="flex mx-auto items-center h-20 justify-between space-x-40 font-bold text-black px-1">
+                    <div className="flex items-center space-x-4">
+                        <Icons.facebook className="w-6 h-6" />
+                        <Icons.twitter className="w-6 h-6" />
+                        <Icons.instagram className="w-6 h-6" />
+                    </div>
+                    <div className="flex items-center justify-between gap-4">
+                        <div className="flex items-center" onClick={goToWhatsApp}>
+                            <Icons.phone className="w-6 h-6 mr-2" />
+                            <h1 className="text-xl">07010876015</h1>
+                        </div>
+                        <div className="flex items-center">
+                            <Icons.mail className="w-6 h-6 mr-2" />
+                            <h1 className="text-xl">info@klick.africa</h1>
+                        </div>
+                    </div>
+                </footer>
+            </main>
+        </>
+    )
+}
